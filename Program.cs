f7b2c1f133fe727751247a5e4ca7b81930ac29bf
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2024-11-30 21:22:07 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class laba3
+{
+    static void Main()
+    {
+        //zad3
+        int c = 0, n, max = 0, num;
+        n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                c += num;
+                if (c > max) max = c;
+                
+            }
+            else
+            {
+                c = 0;
+            }
+        }
+        Console.WriteLine(max);
+    }
+
+}
