0514db0437c1d44f80698edf3a8803cf337eb553
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2024-12-01 20:04:04 +0600

    Create Байбулов ФиТ-242 Лаба5 Задание 2.cs

---
+++ Байбулов ФиТ-242 Лаба5 Задание 2.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization.Formatters;
+class laba5
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите количество элемнтов");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+
+        Console.WriteLine("Введите значения элементов");
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+
+        int k3 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (array[i] % 10 == 3) k3++;
+        }
+        Console.WriteLine("Количество элементов оканчивающихся на тройку равно " + k3);
+    }
+}
