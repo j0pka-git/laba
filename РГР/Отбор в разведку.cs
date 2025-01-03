56985d451dd70ad356ff6e55c080c8727606030c
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2025-01-03 17:33:14 +0600

    Update Отбор в разведку.cs

--- РГР/Отбор в разведку.cs
+++ РГР/Отбор в разведку.cs
@@ -2,7 +2,7 @@
 
 class World
 {
-    static int Group_Counter(int n)
+    static int GrpCount(int n)
     {
         if (n < 3 || n == 4) return 0;
         if (n == 3) return 1;
@@ -10,14 +10,14 @@ class World
         int g1 = n / 2;
         int g2 = n - g1;
 
-        return Group_Counter(g1) + Group_Counter(g2);
+        return GrpCount(g1) + GrpCount(g2);
     }
 
     static void Main()
     {
         int n = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine(Group_Counter(n));
-    } 
+
+        Console.WriteLine(GrpCount(n));
+    }
 
 }
